d28fc8db2ec39d5b0801ad09fad16046ac0c493c
Yugal Agarwal <yugal1434@gmail.com>
2021-07-03 17:59:04 +0530

    second commit

---
+++ clock.py
@@ -0,0 +1,8 @@
+# this clock untill and unless detects ur correct weight it will keep on buzzing
+
+w = float(input())
+
+while w < 68:
+    print("it keeps on buzzing!!")
+if 68 < w < 72:
+    print("Stop the alarm clock.")
